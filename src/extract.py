eaa5a5f2a743014bbf25a2c6cdd4927240e82612
andiigela <andiigela@gmail.com>
2024-10-26 11:57:39 +0200

    Simulate a simple AutoLoader to extract data from raw continously

--- src/extract.py
+++ src/extract.py
@@ -1,31 +1,28 @@
 # dataset is from: https://catalog.data.gov/dataset/heart-disease-mortality-data-among-us-adults-35-by-state-territory-and-county-2019-2021
 import pandas as pd
 import os
+import shutil
 
-def extract_raw_data(raw_folder_path):
-    processed_files = []
-    files = [file for file in os.listdir(raw_folder_path) if file.endswith('.csv')]
-    for file in files:
-        raw_file_path = os.path.join(raw_folder_path, file)
-        bronze_output_data = os.path.join('C:/Users/andig/Desktop/Punim Diplome/Heart-Disease-Data-Pipeline/data/bronze/', f'bronze_{file}')
-        df_raw = pd.read_csv(raw_file_path)    
-        # df_raw.to_csv(bronze_output_data, index=False)
-        # print(bronze_output_data)
-    # try:
-    #     files = [file for file in os.listdir(raw_folder_path) if file.endswith('.csv')]
-    #     for file in files:
-    #         raw_file_path = os.path.join(raw_folder_path, file)
-    #         df_raw = pd.read_csv(raw_file_path)
+def extract_raw_data(raw_folder_path, bronze_folder_path, log_file):
+    processed_files = set()
+    try:
+        files = [file for file in os.listdir(raw_folder_path) if file.endswith('.csv')]
+        for file in files:
+            raw_file_path = os.path.join(raw_folder_path, file)
+            bronze_file_path = os.path.join(bronze_folder_path', f'bronze_{file}')
 
-    #         bronze_output_data = os.path.join('C:/Users/andig/Desktop/Punim Diplome/Heart-Disease-Data-Pipeline/data/bronze/', f'bronze_{file}')
-    #         df_raw.to_csv(bronze_output_data, index=False)        
-    #         processed_files.append(f'{raw_folder_path}/{file}')
-    #         print(f'Raw data from {file} successfully extracted and saved to {bronze_output_data}')
-    #     print(processed_files)
-    #     return processed_files
-    # except Exception as ex:
-    #     print(f"Error extracting data from raw folder: {ex}")
-    #     return
+            shutil.copy(raw_file_path, bronze_file_path)      
+            processed_files.append(f'{raw_folder_path}/{file}')
+        print(f'Simulated AutoLoader has noticed these files: {processed_files}')
+        return processed_files
+    except Exception as ex:
+        print(f"Error extracting data from raw folder: {ex}")
+        return
     
 if __name__ == "__main__":
-    extract_raw_data('C:/Users/andig/Desktop/Punim Diplome/Heart-Disease-Data-Pipeline/data/raw/') 
+    extract_raw_data(
+        'C:/Users/andig/Desktop/Punim Diplome/Heart-Disease-Data-Pipeline/data/raw/',
+        'C:/Users/andig/Desktop/Punim Diplome/Heart-Disease-Data-Pipeline/data/bronze/',
+        'C:/Users/andig/Desktop/Punim Diplome/Heart-Disease-Data-Pipeline/data/processed_file.log'
+        
+        ) 
