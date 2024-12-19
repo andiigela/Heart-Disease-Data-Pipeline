235fb341252d970514a905d1f81b8ddc71fcb498
andiigela <andiigela@gmail.com>
2024-12-19 23:14:46 +0100

    Silver-Transformation: changed levels values in dimension table

--- dbt_airflow/dbt/mydbt_mydbc/models/dim/dim_patient.sql
+++ dbt_airflow/dbt/mydbt_mydbc/models/dim/dim_patient.sql
@@ -12,24 +12,21 @@ WITH dim_patient AS (
             ELSE 'Unknown'
         END AS gender,
         CASE 
-            WHEN ck_mb <= 3 THEN 'Low'
-            WHEN ck_mb > 3 AND ck_mb <= 5 THEN 'Moderate'
-            WHEN ck_mb > 5 AND ck_mb <= 10 THEN 'High'
-            WHEN ck_mb > 10 THEN 'Critical'
+            WHEN ck_mb < 3 THEN 'Low'
+            WHEN ck_mb BETWEEN 3 AND 6 THEN 'Moderate'
+            WHEN ck_mb > 6 THEN 'Critical'
             ELSE 'Unknown'
         END AS patient_ck_mb_level,
-        CASE 
+        CASE
             WHEN patient_troponin < 0.04 THEN 'Low'
-            WHEN patient_troponin >= 0.04 AND patient_troponin < 0.4 THEN 'Moderate'
-            WHEN patient_troponin >= 0.4 AND patient_troponin < 1 THEN 'High'
-            WHEN patient_troponin >= 1 THEN 'Critical'
+            WHEN patient_troponin BETWEEN 0.04 AND 0.4 THEN 'Moderate'
+            WHEN patient_troponin > 0.4 THEN 'Critical'
             ELSE 'Unknown'
         END AS patient_troponin_level,
         CASE
             WHEN patient_blood_sugar < 70 THEN 'Low'
-            WHEN patient_blood_sugar BETWEEN 70 AND 140 THEN 'Normal'
-            WHEN patient_blood_sugar BETWEEN 141 AND 199 THEN 'Elevated'
-            WHEN patient_blood_sugar >= 200 THEN 'Critical'
+            WHEN patient_blood_sugar BETWEEN 70 AND 140 THEN 'Moderate'
+            WHEN patient_blood_sugar > 140 THEN 'Critical'
             ELSE 'Unknown'
         END AS patient_blood_sugar_level,
         CASE
@@ -40,21 +37,18 @@ WITH dim_patient AS (
             WHEN patient_systolic_blood_pressure >= 180 THEN 'Critical'
             ELSE 'Unknown'
         END AS patient_systolic_bp_level,
-        CASE
+       CASE
             WHEN patient_diastolic_blood_pressure < 60 THEN 'Low'
-            WHEN patient_diastolic_blood_pressure BETWEEN 60 AND 80 THEN 'Normal'
-            WHEN patient_diastolic_blood_pressure BETWEEN 81 AND 89 THEN 'Elevated'
-            WHEN patient_diastolic_blood_pressure BETWEEN 90 AND 119 THEN 'High'
-            WHEN patient_diastolic_blood_pressure >= 120 THEN 'Critical'
+            WHEN patient_diastolic_blood_pressure BETWEEN 60 AND 90 THEN 'Moderate'
+            WHEN patient_diastolic_blood_pressure > 90 THEN 'Critical'
             ELSE 'Unknown'
         END AS patient_diastolic_bp_level,
         CASE
             WHEN patient_heart_rate < 60 THEN 'Low'
-            WHEN patient_heart_rate BETWEEN 60 AND 100 THEN 'Normal'
-            WHEN patient_heart_rate BETWEEN 101 AND 120 THEN 'Elevated'
-            WHEN patient_heart_rate > 120 THEN 'Critical'
+            WHEN patient_heart_rate BETWEEN 60 AND 100 THEN 'Moderate'
+            WHEN patient_heart_rate > 100 THEN 'Critical'
             ELSE 'Unknown'
-        END AS patient_heart_rate_level
+        END AS patient_heart_rate_category
     FROM {{ ref('stg_silver_local_health') }}
 )
 SELECT * FROM dim_patient;
