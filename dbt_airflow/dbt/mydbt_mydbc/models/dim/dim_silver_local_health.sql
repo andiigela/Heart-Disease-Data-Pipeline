34f1d4c927f2fecb2004cad6bfaf134b28ce76ca
andiigela <andiigela@gmail.com>
2024-12-20 10:50:18 +0100

    Silver-Transformation: Add new derived columns in dim table

--- dbt_airflow/dbt/mydbt_mydbc/models/dim/dim_silver_local_health.sql
+++ dbt_airflow/dbt/mydbt_mydbc/models/dim/dim_silver_local_health.sql
@@ -28,7 +28,7 @@ WITH dim_silver_local_health AS (
             WHEN patient_blood_sugar BETWEEN 70 AND 140 THEN 'Moderate'
             WHEN patient_blood_sugar > 140 THEN 'Critical'
             ELSE 'Unknown'
-        END AS patient_blood_sugar_level,
+        END AS patient_diabetes_risk,
         CASE
             WHEN patient_systolic_blood_pressure < 90 THEN 'Low'
             WHEN patient_systolic_blood_pressure BETWEEN 90 AND 140 THEN 'Moderate'
@@ -46,7 +46,24 @@ WITH dim_silver_local_health AS (
             WHEN patient_heart_rate BETWEEN 60 AND 100 THEN 'Moderate'
             WHEN patient_heart_rate > 100 THEN 'Critical'
             ELSE 'Unknown'
-        END AS patient_heart_rate_category
+        END AS patient_heart_rate_category,
+        CASE 
+            WHEN patient_systolic_blood_pressure < 120 AND patient_diastolic_blood_pressure < 80 THEN 'Low'
+            WHEN (patient_systolic_blood_pressure BETWEEN 120 AND 139 OR patient_diastolic_blood_pressure BETWEEN 80 AND 89) THEN 'Moderate'
+            WHEN (patient_systolic_blood_pressure BETWEEN 140 AND 159 OR patient_diastolic_blood_pressure BETWEEN 90 AND 99) THEN 'High'
+            WHEN (patient_systolic_blood_pressure >= 160 OR patient_diastolic_blood_pressure >= 100) THEN 'Critical'
+            ELSE 'Unknown'
+        END AS patient_hypertension_risk,
+        CASE 
+            WHEN patient_heart_rate BETWEEN 60 AND 100 
+                AND patient_systolic_blood_pressure BETWEEN 90 AND 120 
+                AND patient_diastolic_blood_pressure BETWEEN 60 AND 80 
+            THEN 'Low'
+        WHEN (patient_heart_rate BETWEEN 101 AND 120 OR patient_systolic_blood_pressure BETWEEN 121 AND 140 OR patient_diastolic_blood_pressure BETWEEN 81 AND 90) THEN 'Moderate'
+        WHEN (patient_heart_rate > 120 OR patient_systolic_blood_pressure BETWEEN 141 AND 180 OR patient_diastolic_blood_pressure BETWEEN 91 AND 110) THEN 'High'
+        WHEN (patient_heart_rate > 180 OR patient_systolic_blood_pressure > 180 OR patient_diastolic_blood_pressure > 110) THEN 'Critical'
+        ELSE 'Unknown'
+        END AS patient_heart_stress_level
     FROM {{ ref('stg_silver_local_health') }}
 )
 SELECT * FROM dim_silver_local_health;
