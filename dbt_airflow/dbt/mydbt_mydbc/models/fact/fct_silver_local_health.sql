bb463b222401d884f2b79dde7a801bc0ca976e75
andiigela <andiigela@gmail.com>
2024-12-20 11:36:40 +0100

    Silver-Transformation: changed categorical to numerical values

--- dbt_airflow/dbt/mydbt_mydbc/models/fact/fct_silver_local_health.sql
+++ dbt_airflow/dbt/mydbt_mydbc/models/fact/fct_silver_local_health.sql
@@ -1,6 +1,7 @@
-/* normalization formula => X - Min/ Max - Min = Res   */
+/* normalization formula => X - Min/ Max - Min = Res, values from 0-1  */
 with fact_normalization as (
     Select 
+        *,
         (patient_heart_rate - MIN(patient_heart_rate) OVER()) / 
         (MAX(patient_heart_rate) OVER () - MIN(patient_heart_rate) OVER ()) AS normalized_patient_heart_rate,
         (patient_systolic_blood_pressure - MIN(patient_systolic_blood_pressure) OVER ()) / 
@@ -14,6 +15,52 @@ with fact_normalization as (
         (patient_troponin - MIN(patient_troponin) OVER()) /
         (MAX(patient_troponin) OVER() - MIN(patient_troponin) OVER()) as normalized_troponin
     FROM {{ ref('dim_silver_local_health') }}
-) 
-Select * FROM fact_normalization
-
+),
+categorical_to_numerical_values AS (
+    Select 
+        *,
+        CASE 
+            WHEN patient_ck_mb_level = 'Low' THEN 0
+            WHEN patient_ck_mb_level = 'Moderate' THEN 1
+            WHEN patient_ck_mb_level = 'Critical' THEN 2
+        END AS patient_ck_mb_level_num,
+        CASE
+            WHEN patient_troponin_level = 'Low' THEN 0
+            WHEN patient_troponin_level = 'Moderate' THEN 1
+            WHEN patient_troponin_level = 'Critical' THEN 2
+        END AS patient_troponin_level_num,
+        CASE
+            WHEN patient_diabetes_risk = 'Low' THEN 0
+            WHEN patient_diabetes_risk = 'Moderate' THEN 1
+            WHEN patient_diabetes_risk = 'Critical' THEN 2
+        END AS patient_diabetes_risk_num,
+        CASE
+            WHEN patient_systolic_bp_level = 'Low' THEN 0
+            WHEN patient_systolic_bp_level = 'Moderate' THEN 1
+            WHEN patient_systolic_bp_level = 'Critical' THEN 2
+        END AS patient_systolic_bp_level_num,
+        CASE
+            WHEN patient_diastolic_bp_level = 'Low' THEN 0
+            WHEN patient_diastolic_bp_level = 'Moderate' THEN 1
+            WHEN patient_diastolic_bp_level = 'Critical' THEN 2
+        END AS patient_diastolic_bp_level_num,
+        CASE
+            WHEN patient_heart_rate_category = 'Low' THEN 0
+            WHEN patient_heart_rate_category = 'Moderate' THEN 1
+            WHEN patient_heart_rate_category = 'Critical' THEN 2
+        END AS patient_heart_rate_category_num,
+        CASE 
+            WHEN patient_hypertension_risk = 'Low' THEN 0
+            WHEN patient_hypertension_risk = 'Moderate' THEN 1
+            WHEN patient_hypertension_risk = 'High' THEN 2
+            WHEN patient_hypertension_risk = 'Critical' THEN 3
+        END AS patient_hypertension_risk_num,
+        CASE 
+            WHEN patient_heart_stress_level = 'Low' THEN 0
+            WHEN patient_heart_stress_level = 'Moderate' THEN 1
+            WHEN patient_heart_stress_level = 'High' THEN 2
+            WHEN patient_heart_stress_level = 'Critical' THEN 3
+        END AS patient_heart_stress_level_num 
+    FROM fact_normalization
+)
+Select * from categorical_to_numerical_values
